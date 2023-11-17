282cc820a19de8af3836a07c3b57f879a8493f66
swimzero <swimzero04@korea.ac.kr>
2023-11-17 19:02:20 +0900

    todo

--- todo/src/App.js
+++ todo/src/App.js
@@ -2,33 +2,50 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  let [modalstate, setModalstate] = useState(new Array(100).fill(false));
+  let [modalState, setModalState] = useState([]);
   let [goals, setGoals] = useState([]);
   let [inp, setInp] = useState('');
+  let [detailInp, setDetailInp] = useState('');
+
+  const handleGoalSubmit = () => {
+    if (inp.trim() !== '') {
+      const newGoal = { text: inp, detail: detailInp, date: new Date() };
+      setGoals([...goals, newGoal]);
+      setInp('');
+      setDetailInp('');
+    }
+  };
 
   return (
     <div className="App">
       <div className='nav'>
-        <h4>일주일 안에 꼭 해야할 5가지</h4>
+        <h4>ToDo List</h4>
       </div>
       <div className='adder'>
-        <h5>목표 설정칸</h5>
-        <input value={inp} onChange={(e) => setInp(e.target.value)} />
-        <button onClick={() => setGoals([...goals, inp])}>목표설정</button>
+        <h5>목표를 설정하세요</h5>
+        <input value={inp} onChange={(e) => setInp(e.target.value)}
+        placeholder='목표 입력' />
+        <input
+          value={detailInp}
+          onChange={(e) => setDetailInp(e.target.value)}
+          placeholder="상세 입력"
+        />
+        <button onClick={handleGoalSubmit}>목표설정</button>
       </div>
 
       {goals.map(function (goal, index) {
         return (
           <div className='basic' key={index}>
-            <h4>{goal}</h4>
-            <p>일자</p>
+            <h4>{goal.text}</h4>
+            <p>작성일: {goal.date.toLocaleDateString()}</p>
             <p onClick={() => {
-              let copy = [...modalstate];
-              copy[index] = !copy[index];
-              setModalstate(copy);
+              setModalState(prevState => ({
+                ...prevState,
+                [index]: !prevState[index]
+              }));
             }}>상세 목표</p>
 
-            {modalstate[index] ? <Modal goal={goal} index={index} key={index} /> : null}
+            {modalState[index] ? <Modal goal={goal} key={index} /> : null}
           </div>
         );
       })}
@@ -39,8 +56,8 @@ function App() {
 let Modal = (props) => {
   return (
     <div>
-      <h4>{props.goal}</h4>
-      <p>뭘 상세정보야 너 머릿속에 있어야지 너가 지키지</p>
+      <h4>상세목표</h4>
+      <p>{props.goal.detail}</p>
     </div>
   );
 }
