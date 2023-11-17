020fc8b793b939c8c3ca05c86e18c12ce64c6a3e
swimzero <swimzero04@korea.ac.kr>
2023-11-17 18:32:52 +0900

    1

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [modalstate, setModalstate] = useState(new Array(100).fill(false));
+  let [goals, setGoals] = useState([]);
+  let [inp, setInp] = useState('');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='nav'>
+        <h4>일주일 안에 꼭 해야할 5가지</h4>
+      </div>
+      <div className='adder'>
+        <h5>목표 설정칸</h5>
+        <input value={inp} onChange={(e) => setInp(e.target.value)} />
+        <button onClick={() => setGoals([...goals, inp])}>목표설정</button>
+      </div>
+
+      {goals.map(function (goal, index) {
+        return (
+          <div className='basic' key={index}>
+            <h4>{goal}</h4>
+            <p>일자</p>
+            <p onClick={() => {
+              let copy = [...modalstate];
+              copy[index] = !copy[index];
+              setModalstate(copy);
+            }}>상세 목표</p>
+
+            {modalstate[index] ? <Modal goal={goal} index={index} key={index} /> : null}
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+let Modal = (props) => {
+  return (
+    <div>
+      <h4>{props.goal}</h4>
+      <p>뭘 상세정보야 너 머릿속에 있어야지 너가 지키지</p>
     </div>
   );
 }
